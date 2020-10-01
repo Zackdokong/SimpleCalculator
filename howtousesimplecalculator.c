53ec3760cc4f188c2b83717819563ba52a5e1ffd
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2020-10-01 16:56:33 +0900

    Create howtousesimplecalculator.c

---
+++ howtousesimplecalculator.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include "MainPage.h"
+
+int howtouse()
+{
+	system("cls");
+	printf("Enter a codenumber to use Simple Calculater\n");
+	system("pause");
+	mainpage();
+	return 0;
+}
