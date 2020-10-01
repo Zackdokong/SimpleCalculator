624d2fc21b2ca2b2206262512d569aa27e96fa27
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2020-10-01 16:59:07 +0900

    Create startpage.c

---
+++ startpage.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "MainPage.h"
+#include <Windows.h>
+
+int start()
+{
+	system("cls");
+	printf("Simple caculater Ver. 0.1.202010011644.10");
+	printf("\n\n\n\n\n\nWELCOME");
+	Sleep(1000);
+	mainpage();
+	return 0;
+}
